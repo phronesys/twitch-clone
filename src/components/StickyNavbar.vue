82ab3498c5abe13317353726e6c3e15f2a79354a
phronesys <daniel.dazarola@gmail.com>
2021-03-02 00:38:48 -0300

    first commit

---
+++ src/components/StickyNavbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <section>
+    <header class="header">
+      <div class="navbar">
+        <svg class="navbar__logo">
+          <use xlink:href="../assets/sprites.svg#icon-twitch"></use>
+        </svg>
+        <div class="navbar__text-box">
+          <p class="navbar__text">Following</p>
+          <p class="navbar__text">Browse</p>
+        </div>
+        <svg class="navbar__hdots">
+          <use xlink:href="@/assets/SVG/bubbles3.svg"></use>
+        </svg>
+      </div>
+    </header>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.header {
+}
+.navbar {
+  display: flex;
+
+  align-items: center;
+
+  &__logo {
+    width: 2rem;
+    fill: #9147ff;
+  }
+  &__text-box {
+    display: flex;
+    margin-left: auto;
+  }
+  &__text {
+  }
+  &__hdots {
+    position: relative;
+    color: purple;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <sticky-navbar></sticky-navbar>
+    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import StickyNavbar from '../components/StickyNavbar';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    StickyNavbar
+  },
+  setup(){
+    
   }
 }
 </script>
